d7255620ff69128a0165414c8feaec4fc675cce6
thulasiprahasan <74705026+thulasiprahasan@users.noreply.github.com>
2021-12-16 20:35:34 +0530

    Add files via upload

---
+++ Treasure Hunt/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "CUPBOARD";
+const accessCode2 = "SINK";
+const accessCode3 = "BED";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("B A R C O D U P", 100,50)
+    fill("lightblue")
+    text("Hint: Getting the first clue it will be joy , the place where you put your toys  !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("N K I S", 700,150)
+    fill("lightblue")
+    text("Hint:  you wash your hands in the _____ !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("D E B", 100,250)
+    fill("lightblue")
+    text("Hint: When you are up walking around , go to the place where you sleep down !!", 100,270)
+
+}
